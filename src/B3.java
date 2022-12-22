54adac99147d3f461d884fa52933d784a37bcb4a
tugbafidann <tugbafidan4545@gmail.com>
2022-12-22 14:12:59 +0300

    v3

---
+++ src/B3.java
@@ -0,0 +1,13 @@
+public class B3 {
+    /*
+    Projemizde önemli bir degisiklik yapmak istiyoruz
+    bu degisikliğe 10 gun çalısacağız
+    degisikligi yapmaya devam ederken orjinal projemizi riske atmamak
+    ve eger son kullanici kullaniyorsa olusabilecek sorunlari engellemek icin
+
+    orjinal proje uzerinde calismak yerine yeni bir BRANCH olusturup
+    o branch'de degisiklikleri yapariz.
+    Calisma sonucunda istenen hedefe ulasailirsek olusturdugumuz
+    branch'i asil branc'e kopyalariz
+     */
+}
