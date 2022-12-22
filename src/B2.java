5cb21c3dc983eaf246fb9a9ede49a7d42898b7a4
tugbafidann <tugbafidan4545@gmail.com>
2022-12-22 13:53:41 +0300

    son versiyon

---
+++ src/B2.java
@@ -0,0 +1,17 @@
+public class B2 {
+    /*
+    Temelde git ve gitHub icin 3 asamadan bahsedebiliriz
+      1- Proje veya klasörumuz ile git'i tanistirma
+            git init
+      2- Proje veya klasörumuz icin versiyon olusturma
+            git add.
+            git commit -m "versiyon adi"
+      3- gitHub ile entegrasyon ve sekronizasyon
+          - ilk defa entegre etmek icin once gitHub'da yeni repo olusturup
+          aradaki son 3 adimi kopyalayip terminal/ CMD ye yapistirin
+          - ilk entegrasyondan sonra her degisiklik yaptigimizda
+              - git add .
+              - git commit -m "versiyon adi"
+              - git push
+     */
+}
